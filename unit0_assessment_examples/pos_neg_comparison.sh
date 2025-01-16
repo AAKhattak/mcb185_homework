4cd6cce272284ef9832121db38019e39fba0d70f
AAKhattak <aakhattak@ucdavis.edu>
2025-01-16 13:19:12 -0800

    unit0_assessment_examples/

--- unit0_assessment_examples/pos_neg_comparison.sh
+++ unit0_assessment_examples/pos_neg_comparison.sh
@@ -1,2 +1,4 @@
-gunzip -c ~/Code/mcb185_homework/ecoli.gff.gz | cut -f 7 | grep "+" | wc -l
-gunzip -c ~/Code/mcb185_homework/ecoli.gff.gz | cut -f 7 | grep "-" | wc -l
+positive_strand=$(gunzip -c ~/Code/mcb185_homework/ecoli.gff.gz | cut -f 7 | grep "+" | wc -l)
+negative_strand=$(gunzip -c ~/Code/mcb185_homework/ecoli.gff.gz | cut -f 7 | grep "-" | wc -l)
+echo "Positive Strand Genes: $positive_strand"
+echo "Negative Strand Genes: $negative_strand"
