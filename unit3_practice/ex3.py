f19bd8d2ac919039c8652a146844306d2a4c647f
AAKhattak <aakhattak@ucdavis.edu>
2025-02-20 15:42:38 -0800

    ex3_assessment

--- unit3_practice/ex3.py
+++ unit3_practice/ex3.py
@@ -9,24 +9,23 @@ def stats(vals):
     var = 0
     mini = vals[0]
     max =  vals[0]
-    print('number of values =', len(vals))
     for val in vals[1:]:
         if val < mini: mini = val
     minmax.append(mini)
     for val in vals[1:]:
        if val > max: max = val
     minmax.append(max)
-    print('[minimum, maximum] =', minmax)  
     for i in vals:
         sum += i
     mean = sum / len(vals)
-    print('mean =', mean)
     for i in vals:
         var += (mean - i)**2
+    print('number of values =', len(vals))
+    print('[minimum, maximum] =', minmax)  
+    print('mean =', mean)
     print('std =', math.sqrt(var / len(vals)))
     for i in vals:
         zscore = 0
         zscore = abs((i-mean) / math.sqrt(var / len(vals)))
-        print(zscore)
-
+        print('zscore =', zscore)
 stats(vals)
